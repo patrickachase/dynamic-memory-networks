7565c993fb59b04c3bea116a1c8854e9d5945a76
Arushi Raghuvanshi <arushi.raghu@gmail.com>
2016-05-26 16:15:10 -0700

    added plotting code

--- python/dynamic_memory_network.py
+++ python/dynamic_memory_network.py
@@ -23,7 +23,6 @@ from params import parse_args
 params = parse_args()
 
 WORD_VECTOR_LENGTH = 50
-VOCAB_LENGTH = 10000
 NUM_CLASSES = 2
 MAX_EPISODES = 3
 MAX_INPUT_SENTENCES = 40
@@ -466,6 +465,14 @@ def run_baseline():
         print "Weights saved"
 
       print 'Total time: {}'.format(time.time() - start)
+
+      outfile = './outputs/lr_' + str(LEARNING_RATE) + '_hs_' + str(HIDDEN_SIZE) +'_e_' + str(MAX_EPOCHS) + '.txt'
+      f = open(outfile, "a")
+      f.write('train_acc, ' + str(training_accuracy) + '\n')
+      f.write('train_loss, ' + str(average_training_loss) + '\n')
+      f.write('val_acc, ' + str(validation_accuracy) + '\n')
+      f.write('val_loss, ' + str(average_validation_loss) + '\n')
+      f.close()
 
     # Compute average loss on testing data with best weights
     saver.restore(sess, '../data/weights/rnn.weights')
---
+++ python/plot_loss_acc.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+
+def plot_from_file(filename):
+  train_loss = []
+  val_loss = []
+  train_acc = []
+  val_acc = []
+
+  for line in open(filename):
+    line_arr = line.strip().split(',')
+    if line_arr[0] == 'train_loss':
+      train_loss.append(float(line_arr[1]))
+    if line_arr[0] == 'val_loss':
+      val_loss.append(float(line_arr[1]))
+    if line_arr[0] == 'train_acc':
+      train_acc.append(float(line_arr[1]))
+    if line_arr[0] == 'val_acc':
+      val_acc.append(float(line_arr[1]))
+
+  train_plot, = plt.plot(xrange(len(train_loss)), train_loss, label='Train loss')
+  val_plot, = plt.plot(xrange(len(val_loss)), val_loss, label = 'Val loss')
+  plt.title('Loss')
+  plt.xlabel('Epoch')
+  plt.ylabel('Loss value')
+  plt.legend(handles=[train_plot, val_plot])
+  plt.show()
+
+  train_acc_plot, = plt.plot(xrange(len(train_acc)), train_acc, label='Train accuracy')
+  val_acc_plot, = plt.plot(xrange(len(val_acc)), val_acc, label = 'Val accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epoch')
+  plt.ylabel('Accuracy value')
+  plt.legend(handles=[train_acc_plot, val_acc_plot])
+  plt.show()
+
+plot_from_file('outputs/lr_0.001_hs_50_e_500.txt')
