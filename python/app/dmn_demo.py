6c208e8f6414628eb2ac641c09f0ff7e84a6b844
Patrick Chase <patrickchase@DN0a22b10c.SUNet>
2016-05-29 21:30:32 -0700

    Add working input and output.

--- python/app/dmn_demo.py
+++ python/app/dmn_demo.py
@@ -1,14 +1,21 @@
 from flask import render_template
 from app import app
+from flask import request
 
+from dmn_with_word_embedding import add_placeholders, convert_to_vectors, RNN, \
+  input_module, question_module, episodic_memory_module, answer_module
+
+
+def get_answer(input, question):
+  return "yes"
 
 @app.route('/')
 @app.route('/dmn_demo')
 def index():
-    user = None
-    posts = None
+    input = request.args.get('input')
+    question = request.args.get('question')
+
     return render_template("dmn_demo.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+                           input=input,
+                           question=question)
 
